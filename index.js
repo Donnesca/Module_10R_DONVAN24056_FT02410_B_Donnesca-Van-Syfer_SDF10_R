addcaeff9ca4c5635800f17fdff48ead24629c39
Donnesca <163658888+Donnesca@users.noreply.github.com>
2024-11-20 18:48:34 +0200

    Added all styling in css, added my image to html and made the needed changes.

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
-})
+const appSettings = {
+  databaseURL: "https://realtime-database-47037-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListinDB = ref(database, "shoppinglist");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(shoppingListinDB, inputValue);
+
+  console.log(inputValue);
+});
